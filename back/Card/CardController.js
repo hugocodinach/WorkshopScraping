8332c247601a59fcdc1c48d634066b5d8efd5609
hugocodinach <hugo.codinach@epitech.eu>
2020-02-12 11:05:39 +0100

    [MERGE]

--- back/Card/CardController.js
+++ back/Card/CardController.js
@@ -1,7 +1,6 @@
 let express = require("express");
 let router = express.Router();
 let bodyParser = require("body-parser");
-let Amazon = require('./Scripts/amazon');
 
 const request = require("request");
 
